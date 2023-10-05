828929031e4203cc1712b0d60dc5295ebd99dcf0
Kařás <98209882+Wlad1slav@users.noreply.github.com>
2023-10-05 14:25:49 +0300

    Update Goods.php

--- Goods.php
+++ Goods.php
@@ -5,20 +5,16 @@ class Goods {
     private string $name; // Назва продукту
     private float $price; // Ціна за 1 шт. продукту
 
-    private Utils $utils;
-
     /**
      * @param int $id
      * @param string $name
      * @param float $price
      */
     public function __construct(int $id, string $name, float $price) {
-        $this->utils = new Utils(); // додаткові утіліти
-
         $this->id = $id;
-        $this->utils->validateName($name, 'Конструктор Goods'); // Utils. Викликає помилку, якщо довжина строки = 0
+        Utils::validateName($name, 'Конструктор Goods'); // Utils. Викликає помилку, якщо довжина строки = 0
         $this->name = $name;
-        $this->price = $this->utils->atLeastFloat($price, 0); // Utils
+        $this->price = Utils::atLeastFloat($price, 0); // Utils
     }
 
     public function getId(): int {
@@ -32,7 +28,7 @@ class Goods {
     }
     public function setName(string $name): void {
         // Встановлює назву товару
-        $this->utils->validateName($name, "setName(string $name)"); // Utils. Викликає помилку, якщо довжина строки = 0
+        Utils::validateName($name, "setName(string $name)"); // Utils. Викликає помилку, якщо довжина строки = 0
         $this->name = $name;
     }
 
@@ -42,7 +38,7 @@ class Goods {
     }
     public function setPrice(float $price): void {
         // Встановлює ціну товару
-        $this->price = $this->utils->atLeastFloat($price, 0); // Utils
+        $this->price = Utils::atLeastFloat($price, 0); // Utils
     }
 
 }
