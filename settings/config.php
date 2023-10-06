6c5ca6827f21931f920bf1d97d45d362a3fdb6c7
Kařás <98209882+Wlad1slav@users.noreply.github.com>
2023-10-06 20:59:25 +0300

    alpha-0.2.1

--- settings/config.php
+++ settings/config.php
@@ -1,7 +1,7 @@
 <?php
 return [
     'Назва ПЗ' => 'Керуючий Єноте',
-    'Версія' => 'alpha-0.2.0',
+    'Версія' => 'alpha-0.2.1',
     "Пошта для зв'язку" => 'vlad.fokin2004.vf@gmail.com',
     'Github Repository' => 'https://github.com/Wlad1slav/Kursaczh-OOP',
     'url_placeholder' => '/Kursaczh%20OOP%20PHP'
