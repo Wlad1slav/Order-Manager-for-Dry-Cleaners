51acd4af36fa4b07b57558937466b1dc45a42920
Kařás <98209882+Wlad1slav@users.noreply.github.com>
2023-10-06 20:56:28 +0300

    Update include.php

--- templates/base/include.php
+++ templates/base/include.php
@@ -1,7 +1,18 @@
 <?php
 include 'styles.php';
-include '../Rights.php';
+
+include '../Repository.php';
+include '../RepositoryTraits.php';
+
 include '../Utils.php';
+
+include '../Goods.php';
+include '../Rights.php';
+include '../User.php';
+include '../Customer.php';
+include '../Product.php';
+include '../Order.php';
+
 
 $config = require '../settings/config.php';
 $sidebar = require '../settings/sidebar_rows.php';
