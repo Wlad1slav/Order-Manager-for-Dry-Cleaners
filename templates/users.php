f7db611d8bca775c3e5c052b3bef2da0a4fc6d32
Kařás <98209882+Wlad1slav@users.noreply.github.com>
2023-10-06 20:56:24 +0300

    Сверстана сторінка users Користувачі

--- templates/users.php
+++ templates/users.php
@@ -4,23 +4,24 @@ $pageTitle = "Користувачі";
 
 include('base/header.php');
 include('base/sidebar.php');
+
+const COLUMNS = ['username', 'rights'];
 ?>
 
 <div>
+    <!--Форма створення користувачів-->
     <form method="post" class="userForm">
-        <h3>User</h3>
+        <h1>Корристувачі</h1>
+        <h2>Форма створення</h2>
         <label>
-            Username:
-            <input name="username" type="text" required>
+            <input name="username" type="text" placeholder="Логін" required>
         </label>
 
         <label>
-            Password:
-            <input name="password" type="password" required>
+            <input name="password" type="password" placeholder="Пароль" required>
         </label>
 
         <label>
-            Rights:
             <select name="rights" required>
                 <?php
                 $rights = require '../settings/rights_list.php';
@@ -30,13 +31,41 @@ include('base/sidebar.php');
             </select>
         </label>
 
-        <input type="submit" value="ADD">
+        <input type="submit" value="Додати">
 
     </form>
+</div>
+
+<div>
+    <!--ТАБЛИЦЯ КОРИСТУВАЧІВ-->
+    <h2>Список</h2>
+    <table>
+        <thead>
+            <tr class="firsts-row">
+                <th>ID</th>
+                <th>Username</th>
+                <th>Рівень прав</th>
+            </tr>
+        </thead>
+        <thead>
+            <?php
+            foreach(User::getAll() as $user) {
+                echo '<tr>';
+
+                $column = 0;
+                foreach ($user as $value) {
+                    if ($column != 2)
+                        echo "<th>$value</th>";
+                    $column++;
+                }
+                echo "<th><a href='edit/". $user['id'] . "'>Редагувати</a></th>";
+                echo "<th><a class='red-text' href='delete/". $user['id'] . "'>X</a></th>";
+                echo '</tr>';
+            }
+            ?>
+        </thead>
+    </table>
     
-    <ul>
-        
-    </ul>
 </div>
 
 <?php include('base/footer.php'); ?>
