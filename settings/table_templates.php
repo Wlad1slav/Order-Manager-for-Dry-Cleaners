615b11dbe5628ff9eea854705c5546c8f38e1b64
Kařás <98209882+Wlad1slav@users.noreply.github.com>
2023-10-09 12:35:36 +0300

    Update table_templates.php

--- settings/table_templates.php
+++ settings/table_templates.php
@@ -4,6 +4,6 @@ $config = require 'db_config.php';
 $db = $config['dbname'];
 return [
     'orders'=>'',
-    'users'=>"CREATE TABLE `$db`.`users` (`id` INT NOT NULL AUTO_INCREMENT , `username` VARCHAR(25) NOT NULL , `password` VARCHAR(60) NOT NULL , `id_rights` INT NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;",
-    'customers'=>"CREATE TABLE `$db`.`customers` (`full_name` VARCHAR(30) NOT NULL , `phone_number` VARCHAR(20) NOT NULL , `discount` FLOAT(3,1) NOT NULL DEFAULT '0' , `id` INT NOT NULL AUTO_INCREMENT , PRIMARY KEY (`id`)) ENGINE = InnoDB;"
+    'users'=>"CREATE TABLE `$db`.`users` (`id` INT NOT NULL AUTO_INCREMENT , `username` VARCHAR(25) NOT NULL , `password` VARCHAR(60) NOT NULL , `id_rights` INT NOT NULL , PRIMARY KEY (`id`), UNIQUE `username` (`username`)) ENGINE = InnoDB;",
+    'customers'=>"CREATE TABLE `$db`.`customers` (`id` INT NOT NULL AUTO_INCREMENT , `name` VARCHAR(30) NOT NULL , `phone` VARCHAR(20) NULL , `discount` FLOAT(3,1) NOT NULL DEFAULT '0' , `advertising_company` VARCHAR(200) NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;"
 ];
