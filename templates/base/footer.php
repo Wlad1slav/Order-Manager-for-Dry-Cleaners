c9187dc9f0ae7809d4b8b865cc40da3cf47a2fb4
Kařás <98209882+Wlad1slav@users.noreply.github.com>
2023-10-10 16:01:03 +0300

    Update footer.php

--- templates/base/footer.php
+++ templates/base/footer.php
@@ -1,10 +1,14 @@
 </div> <!--content -->
 </div> <!--page-container -->
+<?php
+/**
+ * @var $config
+ */
+?>
 <footer>
-    <p>© 2023 Керуючий Єноте. Усі права захищені.</p>
-    <p>Зв'язатися: <?php echo $config["Пошта для зв'язку"] ?></p>
+    <p>(Ↄ) 2023 <?php echo $config["Назва ПЗ"] ?> :: <i> <b>усі</b> права не захищені, авторського права не існує </i></p>
     <div>
-        <a href="<?php echo $config['Github Repository'] ?>" target="_blank"><?php echo $config['Github Repository'] ?></a>
+        <a href="<?php echo $config['Github Repository'] ?>" target="_blank"><span class="cta-underline"><?php echo $config['Github Repository'] ?></span></a>
         <i style="float: right">Програмне забезпечення підготовлено в якості курсової роботи <br> для Європейського університету, м. Київ.</i>
     </div>
 </footer>
