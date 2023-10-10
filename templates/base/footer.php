a6720a5220aa0127429d08f3238315dbe92e7b21
Kařás <98209882+Wlad1slav@users.noreply.github.com>
2023-10-10 12:38:04 +0300

    Додана інформація в header і footer
    Покращені стилі

--- templates/base/footer.php
+++ templates/base/footer.php
@@ -3,7 +3,10 @@
 <footer>
     <p>© 2023 Керуючий Єноте. Усі права захищені.</p>
     <p>Зв'язатися: <?php echo $config["Пошта для зв'язку"] ?></p>
-    <a href="<?php echo $config['Github Repository'] ?>" target="_blank"><?php echo $config['Github Repository'] ?></a>
+    <div>
+        <a href="<?php echo $config['Github Repository'] ?>" target="_blank"><?php echo $config['Github Repository'] ?></a>
+        <i style="float: right">Програмне забезпечення підготовлено в якості курсової роботи <br> для Європейського університету, м. Київ.</i>
+    </div>
 </footer>
 </body>
 </html>
--- templates/base/header.php
+++ templates/base/header.php
@@ -5,12 +5,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $pageTitle . ' - ' . $config['Назва ПЗ']; ?></title>
     <script src="../static/javascript/utils.js"></script>
+    <script src="../static/javascript/convertTableToCSV.js"></script>
 </head>
 <body>
 
 <div class="header">
 
-    <h1><?php echo $config['Назва ПЗ']?></h1> <p><?php echo $config['Версія']?></p>
+    <h1><?php echo $config['Назва ПЗ']?></h1>
+    <p><?php echo $config['Версія']?></p>
+    <h1 class="UA">.UA</h1>
 
 </div>
 
