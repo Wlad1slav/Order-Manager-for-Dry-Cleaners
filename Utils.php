9dda47934f6508e6c310b0a1b8d4b0db470b120e
Kařás <98209882+Wlad1slav@users.noreply.github.com>
2023-10-11 11:59:07 +0300

    Доданий метод, що повертає посилання на сторінку, на якій знаходиться користувач

--- Utils.php
+++ Utils.php
@@ -1,18 +1,30 @@
 <?php
 
 class Utils {
+
     public static function atLeast(int $value, int $num): int {
         // Перевіряє, чи число value більше num. Якщо ні, то повертає num
         if ($num > $value) return $num;
         return $value;
     }
+
     public static function atLeastFloat(float $value, int $num): float {
         // Перевіряє, чи число value більше num. Якщо ні, то повертає num
         if ($num > $value) return $num;
         return $value;
     }
+
     public static function validateName(string $name, string $placeError = 'Error'): void {
         if (strlen($name) == 0)
             throw new InvalidArgumentException("$placeError: Очікується непуста строка назви товару");
     }
+
+    public static function getCurrentUri(): string {
+        // Повертає повене посилання на сторінку
+        $scheme = $_SERVER['HTTPS'] ?? 'off' == 'on' ? 'https' : 'http';
+        $host = $_SERVER['HTTP_HOST'];
+        $uri = $_SERVER['REQUEST_URI'];
+        return $scheme . '://' . $host . $uri;
+    }
+
 }
