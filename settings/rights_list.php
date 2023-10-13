3b0521a57f19931b7134add377927503d518b2bf
Kařás <98209882+Wlad1slav@users.noreply.github.com>
2023-10-13 16:49:57 +0300

    Update rights_list.php

--- settings/rights_list.php
+++ settings/rights_list.php
@@ -1,7 +1,22 @@
 <?php
 
+$all = [
+    'homepage' =>   ['view'],                                                       // Головна сторінка
+    'orders' =>     ['view', 'create', 'delete', 'edit', 'export', 'import'],       // Замовлення
+    'customers' =>  ['view', 'create', 'delete', 'edit', 'export', 'import'],       // Клієнти
+    'users' =>      ['view', 'create', 'delete'],                                   // Сервіси
+    'products' =>   ['view', 'export', 'import'],                                   // Користувачі
+    'analytic' =>   ['view', 'export'],                                             // Аналітика
+    'settings' =>   ['view', 'edit'],                                               // Налаштування
+];
+
 //include 'Rights.php';
 return [
-    new Rights(1, 'root', []),
-    new Rights(2, 'worker', [])
+    new Rights(1, 'root', $all),        // Супер користувач
+    new Rights(2, 'worker', [           // Працівник
+        'homepage' =>   ['view'],
+        'orders' =>     ['view', 'create', 'edit'],
+        'customers' =>  ['view', 'create', 'edit'],
+        'products' =>   ['view'],
+    ])
 ];
