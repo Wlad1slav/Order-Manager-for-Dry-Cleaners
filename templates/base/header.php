10f9334f8f8caa19bdacde015ee1691b8cfc1d9c
Kařás <98209882+Wlad1slav@users.noreply.github.com>
2023-10-18 14:47:04 +0300

    Додана лого в header

--- templates/base/header.php
+++ templates/base/header.php
@@ -23,6 +23,7 @@
 
 <div class="header">
 
+    <img src="/static/images/logo.png" alt="logo">
     <h1><?php echo $config['Назва ПЗ']?></h1>
     <p><?php echo $config['Версія']?></p>
     <h1 class="UA">.UA</h1>
