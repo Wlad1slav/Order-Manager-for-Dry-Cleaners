10c4bb7b838c1e54231bf00f48cb243e06be1aca
Kařás <98209882+Wlad1slav@users.noreply.github.com>
2023-10-24 13:46:59 +0300

    alpha-0.4.0

--- settings/config.php
+++ settings/config.php
@@ -1,7 +1,7 @@
 <?php
 return [
     'Назва ПЗ' => 'Керуючий Єнот',
-    'Версія' => 'alpha-0.3.7',
+    'Версія' => 'alpha-0.4.0',
     "Пошта для зв'язку" => 'vlad.fokin2004.vf@gmail.com',
     'Github Repository' => 'https://github.com/Wlad1slav/Kursaczh-OOP',
     'ROOT_FOLDER' => 'D:\DEV\Ampps\www\\',
