8338148759c77d6f14c88f9147723b16cf825b73
Reißieck <98209882+Wlad1slav@users.noreply.github.com>
2023-11-12 16:21:02 +0200

    Update config.php

--- settings/config.php
+++ settings/config.php
@@ -4,5 +4,4 @@ return [
     'Версія' => 'alpha-0.5.8',
     "Пошта для зв'язку" => 'vlad.fokin2004.vf@gmail.com',
     'Github Repository' => 'https://github.com/Wlad1slav/Kursaczh-OOP',
-    'ROOT_FOLDER' => 'D:\DEV\Ampps\www\\',
 ];
