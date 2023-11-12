20df48ca4b759f377447a95702c132ff71883840
Reißieck <98209882+Wlad1slav@users.noreply.github.com>
2023-11-12 16:24:31 +0200

    Додаткові перевірки при запуску додатку
    Якщо не існує бази даних, то вона створюється.
    
    Якщо не існує суперукористувача, то він створюється.

--- index.php
+++ index.php
@@ -8,8 +8,24 @@ error_reporting(E_ALL);
 global $DIR;
 $DIR = __DIR__;
 
-require_once 'Router.php';
+// Перевірка, чи існує база даних
+require_once 'Repository.php';
+$dbconfig = json_decode(file_get_contents(Repository::DB_CONFIG_PATH), true) ?? [];
+if (!Repository::checkDB($dbconfig['dbname']))
+    Repository::createDB($dbconfig['dbname']);
+$dbconfig = null;
 
+// Перевірка, чи існує суперкористувач. Якщо ні - він створюється
+require_once 'User.php';
+require_once 'Rights.php';
+if (!User::isExist(1)) {
+    $user = new User('root', 'root', Rights::get(1));
+    $user->save();
+}
+
+
+// Завантаження маршрутизатору
+require_once 'Router.php';
 global $router;
 $router = new Router();
 require_once 'settings/routes.php';
