5dd3f594a782d0aa576cd3fb7996850644c9064d
Reißieck <98209882+Wlad1slav@users.noreply.github.com>
2023-11-16 19:14:22 +0200

    Прибраний зайвий контейнер

--- templates/app-settings/additionalField.php
+++ templates/app-settings/additionalField.php
@@ -1,17 +1,14 @@
 
-<div class="container-margin">
-    <h3>Додаткові поля замовлення</h3>
+<h3>Додаткові поля замовлення</h3>
 
-    <?php
-    // Якщо таблиця додаткових полів не пуста, то йдеться виклик її тегу
-    global $router;
-    $fieldsArray = new ProductAdditionalFields();
+<?php
+// Якщо таблиця додаткових полів не пуста, то йдеться виклик її тегу
+global $router;
+$fieldsArray = new ProductAdditionalFields();
 
-    if (count($fieldsArray->getFields()) > 0)
-        include "additionalFieldTableTag.php"
+if (count($fieldsArray->getFields()) > 0)
+    include "additionalFieldTableTag.php"
 
-    ?>
+?>
 
-    <?php include 'additionalFieldCreate.php'; ?> <!-- Форма створення нового поля замовлення -->
-
-</div>
+<?php include 'additionalFieldCreate.php'; ?> <!-- Форма створення нового поля замовлення -->
--- templates/app-settings/additionalFieldCreate.php
+++ templates/app-settings/additionalFieldCreate.php
@@ -1,25 +1,24 @@
-<div class="container-margin">
-    <p><b>Додати нове поле</b></p>
 
-    <form class="fields-create" method="post">
-        <label for="fieldName">Назва поля</label>
-        <input type="text" name="fieldName" id="fieldName" required>
+<p><b>Додати нове поле</b></p>
 
-        <label for="fieldType">Тип поля</label>
-        <select name="fieldType" id="fieldType" required>
-            <?php
-            foreach (ProductAdditionalFields::TYPES as $type)
-                echo "<option value='$type'>$type</option>"
-            ?>
-        </select>
+<form class="fields-create" method="post">
+    <label for="fieldName">Назва поля</label>
+    <input type="text" name="fieldName" id="fieldName" required>
 
-        <label for="defaultFieldValue">Значення за замовчуванням</label>
-        <input type="text" name="defaultFieldValue" id="defaultFieldValue">
+    <label for="fieldType">Тип поля</label>
+    <select name="fieldType" id="fieldType" required>
+        <?php
+        foreach (ProductAdditionalFields::TYPES as $type)
+            echo "<option value='$type'>$type</option>"
+        ?>
+    </select>
 
-        <label for="availableValues">Швидкий вибір значень (через кому)</label>
-        <textarea name="availableValues" id="availableValues"></textarea>
-        <div>
-            <input type="submit" value="Додати">
-        </div>
-    </form>
-</div>
+    <label for="defaultFieldValue">Значення за замовчуванням</label>
+    <input type="text" name="defaultFieldValue" id="defaultFieldValue">
+
+    <label for="availableValues">Швидкий вибір значень (через кому)</label>
+    <textarea name="availableValues" id="availableValues"></textarea>
+    <div>
+        <input type="submit" value="Додати">
+    </div>
+</form>
