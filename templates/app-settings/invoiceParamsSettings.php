cec537eab2c6cad6b260dd4ee13b388a1b23aff7
Reißieck <98209882+Wlad1slav@users.noreply.github.com>
2023-12-19 18:27:44 +0200

    Update invoiceParamsSettings.php

--- templates/app-settings/invoiceParamsSettings.php
+++ templates/app-settings/invoiceParamsSettings.php
@@ -5,10 +5,12 @@ global $router;
 
 <p>Які параметри ви хочете використовувати в квитанціях?</p>
 
-<input oninput="checkInput(false)" type="radio" id="old-relevant" name="relevant-or-no" value="old">
+<input oninput="checkInput(false)" type="radio" id="old-relevant" name="relevant-or-no" value="old"
+        <?php if (!Invoice::getJsonConfigElement('Current')) echo 'checked' ?>>
 <label for="old-relevant">Релевантні на момент створення замовлення (рекомендується)</label>
 <br>
-<input oninput="checkInput(true)" type="radio" id="relevant" name="relevant-or-no" value="current">
+<input oninput="checkInput(true)" type="radio" id="relevant" name="relevant-or-no" value="current"
+    <?php if (Invoice::getJsonConfigElement('Current')) echo 'checked' ?>>
 <label for="relevant">Релевантні зараз (ті, що використовуваються зараз)</label>
 
 <p id="error"></p>
