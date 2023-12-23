e1ee59967cb6d11334bddb0a5f0b9272a5263573
Reißieck <98209882+Wlad1slav@users.noreply.github.com>
2023-12-23 14:55:22 +0200

    ugrade методу getAll і створення методу getInfo
    Через метод getAll тепер можна встановити ключі для масиву таблиці, що була отримана з бази даних.
    
    getInfo звертається до бази даних і повертає масив з інформацією о рядку, без створення об'єкту.

--- RepositoryTraits.php
+++ RepositoryTraits.php
@@ -18,13 +18,32 @@ trait RepositoryTraits {
         $this->repository->removeRow($this->id);
     }
 
-    public static function getAll(): array {
+    public static function getAll($key = null): array {
         // Повертає масив усіх замовлень
         $repository = new Repository(self::TABLE, self::COLUMNS);
+        $data = $repository->getAll();
+
+        if ($key !== null) {
+            // Якщо був заданий ключ, з яким бажано отримати масив таблиці SQL
+            // При $eky = 'id', новим ключом кожного елементу стане значення id
+            $reformattedData = [];
+            foreach ($data as $element)
+                $reformattedData[$element[$key]] = $element;
+            return $reformattedData;
+        }
+
+
+
         return $repository->getAll();
     }
 
+    public static function getInfo(int $id): ?array {
+        // Статичний метод, що повертає масив з інформацією о об'єкті. РЕКОМЕНДОВАНИЙ!
+        $repository = new Repository(self::TABLE, self::COLUMNS);
+        return $repository->getRow($id);
+    }
+
     // Метод, що повертає об'єкт замовлення
-    abstract static public function get(?int $id = null, ?string $name = null);
+    abstract static public function get(?int $id = null, ?string $name = null); // Повертає саме об'єкт, неоптимізований метод
     abstract public function getValues(): array;    // Метод, що повертає масив усіх даних, які потрібно ввести у базу даних
 }
