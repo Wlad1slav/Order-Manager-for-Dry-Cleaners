497e113583aaee26ffab34a32b3d34fdae33eb1d
Reißieck <98209882+Wlad1slav@users.noreply.github.com>
2024-01-08 16:45:15 +0200

    Зробив сторінку більш універсальною. Можна дивитися замовлення за певниj період.

--- templates/analytic/ordersByDate.php
+++ templates/analytic/ordersByDate.php
@@ -4,7 +4,9 @@ global $DIR;
 include("$DIR/templates/base/include.php");
 
 $date = $_GET['date'];
-$pageTitle = "Замовлення за $date";
+$pageTitle = "Замовлення {$_GET['status']} за $date";
+if (isset($_GET['period']))
+    $pageTitle .= " / {$_GET['period']}";
 include("$DIR/templates/base/header.php");
 include("$DIR/templates/base/sidebar.php");
 
@@ -13,20 +15,30 @@ global $router;
 if (!isset($analytic))
     $analytic = new Analytic();
 
-$orders = $analytic->getAllOrdersByDate($date, $_GET['field'])['orders'] ?? [];
+// Якщо за посиланням є параметр period, в таблиці будуть показуватися замовлення за період з date по period
+if (isset($_GET['period'])) {
+    $orders = $analytic->getAllOrdersForPeriod(
+        ['start' => $_GET['date'], 'end' => $_GET['period']],
+        $_GET['field']
+    )['orders'] ?? [];
+}
+// Якщо за посиланням немає параметру period, в таблиці замовлень будуть показуватися тільки замовлення за певниj день
+else {
+    $orders = $analytic->getAllOrdersByDate($date, $_GET['field'])['orders'] ?? [];
+}
 
-echo "<h1>Замовлення {$_GET['status']} за $date</h1>";
+echo "<h1>$pageTitle</h1>";
 ?>
 
 <script>
-    function f() {
+    function changeDate() {
         // Проводить редірект на сторінку з іншим параметром date
 
         // Отримуємо поточний URL
         let currentUrl = window.location.href;
 
         // Встановлюємо нову дату, яку хочемо вставити
-        let newDate = document.getElementById('date').value;
+        let newDate = document.getElementById('date-start').value;
 
         // Замінюємо старий параметр дати новим
         let newUrl = currentUrl.replace(/(date=)[^&]+/, '$1' + newDate);
@@ -34,8 +46,33 @@ echo "<h1>Замовлення {$_GET['status']} за $date</h1>";
         // Змінюємо адресу в браузері на новий URL
         window.location.href = newUrl;
     }
+
+    function changePeriod() {
+        // Проводить редірект на сторінку з іншим параметром date
+
+        // Отримуємо поточний URL
+        let currentUrl = window.location.href;
+
+        // Встановлюємо нову дату, яку хочемо вставити
+        let newDate = document.getElementById('date-end').value;
+
+        // Замінюємо старий параметр дати новим
+        let newUrl = currentUrl.replace(/(period=)[^&]+/, '$1' + newDate);
+
+        // Змінюємо адресу в браузері на новий URL
+        window.location.href = newUrl;
+    }
 </script>
 
-<input onchange="f()" type="date" id="date" value="<?php echo $date; ?>">
+<label for="date-start">I крапка</label>
+<input onchange="changeDate()" type="date" id="date-start" value="<?php echo $date; ?>">
+
+<?php
+if (isset($_GET['period'])){
+    $dinput = '<br><label for="date-end">II крапка</label>';
+    $dinput .= "<input onchange='changePeriod()' type='date' id='date-end' value='{$_GET['period']}'>";
+    echo $dinput;
+}
+?>
 
 <?php include "$DIR/templates/orders/table.php"; ?>
