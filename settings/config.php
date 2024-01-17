12f08b42ee9d7c6344765034d9f957d96b2acc33
Reißieck <98209882+Wlad1slav@users.noreply.github.com>
2024-01-17 11:07:58 +0200

    beta-1.0

--- settings/config.php
+++ settings/config.php
@@ -1,7 +1,7 @@
 <?php
 return [
     'Назва ПЗ' => 'Керуючий Єнот',
-    'Версія' => 'alpha-0.9',
+    'Версія' => 'beta-1.0',
     "Пошта для зв'язку" => 'vlad.fokin2004.vf@gmail.com',
     'Github Repository' => 'https://github.com/Wlad1slav/Kursaczh-OOP',
 ];
